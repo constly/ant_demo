ad4f9ed3c9d679219b2b0f735b0999c2adaaacea
constly <constly@foxmail.com>
2024-01-24 21:11:40 +0800

    ImGui基础控件完善中，进度50%

--- pkg/game.demo/imgui/imgui_02.lua
+++ pkg/game.demo/imgui/imgui_02.lua
@@ -19,16 +19,13 @@ local input_content = {text = 'input'}
 
 local flag = {}
 local icon_btn
-local tbComboList = {"峨眉山", "青城山", "西岭雪山", "稻城亚丁", "都江堰(感觉买门票不划算)"}
-local cur_combo = tbComboList[1]
-local radio_flag = 1
-local progress_value = 0.01
 
 local start_x = 50
 local headlen = 100 + start_x;
 local start_x_2 = 500
 local headlen2 = start_x_2 + 100
 local line_space_y = 10
+local tbDataList = {}
 
 -- 当进入示例时
 function system.on_entry()
@@ -38,38 +35,57 @@ function system.on_entry()
     end
 end
 
+local draw_line = function(head1, body1, head2, body2)
+    ImGui.Text(head1)
+    ImGui.SameLine(headlen)
+    body1()
+
+    if head2 and body2 then
+        ImGui.SameLine(start_x_2)
+        ImGui.Text(head2)
+        ImGui.SameLine(headlen2)
+        body2()
+    end
+end
+
 function system.data_changed()
     local start = mgr.get_content_start()
     ImGui.SetNextWindowPos(start.x, start.y)            
     ImGui.SetNextWindowSize(1000, 500)
     if ImGui.Begin("wnd_body", ImGui.Flags.Window {"NoResize", "NoMove", "NoTitleBar", "NoScrollbar", "NoBringToFrontOnFocus", "NoBackground"}) then 
-        for i = 1, 4 do 
+        local n = math.ceil(#tbDataList / 2)
+        for i = 1, n do 
             if i > 1 then 
                 ImGui.Dummy(1, line_space_y)
                 ImGui.NewLine()
             end
             ImGui.SameLine(start_x)
-            system['draw_line_' .. i]()
+            local data1 = tbDataList[i * 2 - 1]
+            local data2 = tbDataList[i * 2] or {}
+            draw_line(data1[1], data1[2], data2[1], data2[2])
             ImGui.Separator()
         end
     end
     ImGui.End()
 end
 
-local draw_line = function(head1, body1, head2, body2)
-    ImGui.Text(head1)
-    ImGui.SameLine(headlen)
-    body1()
+local tbComboList = {"峨眉山", "青城山", "西岭雪山", "稻城亚丁", "都江堰(感觉买门票不划算)"}
+local cur_combo = tbComboList[1]
+local cur_list_box = 1
+local radio_flag = 1
+local progress_value = 0.01
 
-    ImGui.SameLine(start_x_2)
-    ImGui.Text(head2)
-    ImGui.SameLine(headlen2)
-    body2()
-end
+local drag_int_value = { [1] = 50, min = 0, max = 120, format = "%d%%" }
+local drag_float_value = { [1] = 0.5, min = 0, max = 10, speed = 0.1, format = "%d%%" }
+local slider_int_value = { [1] = 20, min = 0, max = 100 }
+local slider_float_value = { [1] = 0.625, min = 0, max = 1 }
+local list_box_value = { [1] = "AA", [2] = "BB", [3] = "CC", current = 1, height = 3 }
 
--- 第一行
-function system.draw_line_1()
-    local func1 = function()
+function system.init_world()
+    local register = function(label, func)
+        table.insert(tbDataList, {label, func})
+    end
+    register("Button:", function()
         if ImGui.Button("按钮##btn_1", 60, 23) then     
             flag[1] = not flag[1]
         end
@@ -77,8 +93,8 @@ function system.draw_line_1()
             ImGui.SameLine()                            
             ImGui.Text("我被你点中咯")
         end
-    end 
-    local func2 = function()
+    end)
+    register("RadioButton:", function()
         if ImGui.RadioButton("男生##radio_id_1", radio_flag == 1) then 
             radio_flag = 1
         end
@@ -86,13 +102,8 @@ function system.draw_line_1()
         if ImGui.RadioButton("女生##radio_id_2", radio_flag == 2) then 
             radio_flag = 2
         end
-    end
-    draw_line("默认按钮:", func1, "单选按钮:", func2)
-end
-
--- 第二行
-function system.draw_line_2()
-    local func1 = function()
+    end)
+    register("ImageButton:", function()
         if ImGui.ImageButton("##btn_2",  textureman.texture_get(icon_btn.id), 60, 23) then 
             flag[2] = not flag[2]
         end
@@ -100,82 +111,96 @@ function system.draw_line_2()
             ImGui.SameLine(); 
             ImGui.Text("我被你点中咯") 
         end
-    end 
-    local func2 = function()
-        progress_value = progress_value + 0.01
-        if progress_value > 1 then 
-            progress_value = 0 
+    end)
+    register("Image:", function()
+        ImGui.Image(textureman.texture_get(icon_btn.id), 35, 35)
+        if ImGui.IsItemHovered() and ImGui.BeginTooltip() then
+            ImGui.Text("你看这个饼，它又大又圆。")
+            ImGui.EndTooltip()
         end
-        ImGui.ProgressBar(progress_value, 120, 22, "嘿嘿") 
-    end
-    draw_line("图片按钮:", func1, "进度条:", func2)
-end 
-
--- 第三行
-local drag_int_value = {
-    [1] = 50,
-    min = 0,
-    max = 120,
-    format = "%d%%"
-}
-function system.draw_line_3()
-    local func1 = function()
+    end)
+    register("InputText:", function()
         ImGui.SetNextItemWidth(150)
         if ImGui.InputText("##input_3", input_content) then 
             print("输入", input_content.text)
         end
-        if ImGui.IsItemHovered() then
-            if ImGui.BeginTooltip() then
-                ImGui.Text(tostring(input_content.text))
-                ImGui.EndTooltip()
-            end
+        if ImGui.IsItemHovered() and ImGui.BeginTooltip() then
+            ImGui.Text(tostring(input_content.text))
+            ImGui.EndTooltip()
         end
-    end
-    local func2 = function()
-        ImGui.SameLine(headlen2)
-        ImGui.SetNextItemWidth(150)
-        if ImGui.DragInt("##drag_int_1", drag_int_value) then
-            print("drag int" .. drag_int_value[1])
-        end
-    end
-    draw_line("文本输入:", func1, "拖动Int:", func2)
-end
-
--- 第四行
-local drag_float_value = {
-    [1] = 0.5,
-    min = 0,
-    max = 10,
-    speed = 0.1,
-    format = "%d%%"
-}
-function system.draw_line_4()
-    local func1 = function()
+    end)
+    register("Combo:", function()
         ImGui.SetNextItemWidth(150)
         if ImGui.BeginCombo("##combo_4", cur_combo) then
             for i, name in ipairs(tbComboList) do
                 if ImGui.Selectable(name, name == cur_combo) then
                     cur_combo = name
                 end
-                if ImGui.IsItemHovered() then
-                    if ImGui.BeginTooltip() then
-                        ImGui.TextWrapped(name .. "的描述", (i - 1) * 40 + 10)
-                        ImGui.EndTooltip()
-                    end
+                if ImGui.IsItemHovered() and ImGui.BeginTooltip() then
+                    ImGui.TextWrapped(name .. "的描述", (i - 1) * 40 + 10)
+                    ImGui.EndTooltip()
                 end
             end
             ImGui.EndCombo()
         end
-    end
-    local func2 = function()
+    end)
+    register("DragInt:", function()
+        ImGui.SetNextItemWidth(150)
+        if ImGui.DragInt("##drag_int_1", drag_int_value) then
+            print("drag int" .. drag_int_value[1])
+        end
+    end)
+    register("DragFloat:", function()
         ImGui.SetNextItemWidth(150)
         if ImGui.DragFloat("##drag_float_11", drag_float_value) then
             print("drag float", drag_float_value[1])
         end
-    end
-    draw_line("下拉选择:", func1, "拖动Float:", func2)    
-end 
-
--- 第五行
-function system.draw_line_5()
+    end)
+    register("SliderInt:", function()
+        ImGui.SetNextItemWidth(150)
+        ImGui.SliderInt("##slider_line_0501", slider_int_value)
+    end)
+    register("SliderFloat:", function()
+        ImGui.SetNextItemWidth(150)
+        ImGui.SliderFloat("##slider_line_0502", slider_float_value)
+    end)
+    register("帮助:", function()
+        ImGui.TextDisabled("(?)");
+        if ImGui.IsItemHovered() and ImGui.BeginTooltip() then 
+            ImGui.PushTextWrapPos(16 * 35);
+            ImGui.Text("黄河远上白云间，一片孤城万仞山。\n羌笛何须怨杨柳，春风不度玉门关。");
+            ImGui.PopTextWrapPos();
+            ImGui.EndTooltip();
+        end
+    end)
+    register("ProgressBar:", function()
+        progress_value = progress_value + 0.01
+        if progress_value > 1 then 
+            progress_value = 0 
+        end
+        ImGui.ProgressBar(progress_value, 150, 22, "嘿嘿") 
+    end)
+    register("ListBox:", function()
+        ImGui.SetNextItemWidth(150)
+        if ImGui.ListBox("##listbox", list_box_value) then 
+            print("list is", list_box_value.current)
+        end
+    end)
+    register("BeginListBox:", function()
+        if ImGui.BeginListBox("##begin_list_box", 150, 70) then 
+            for i, name in ipairs(tbComboList) do 
+                if ImGui.Selectable(name, i == cur_list_box) then 
+                    cur_list_box = i
+                end
+                if i == cur_list_box then 
+                    ImGui.SetItemDefaultFocus();
+                end
+                if ImGui.IsItemHovered() and ImGui.BeginTooltip() then
+                    ImGui.Text(name)
+                    ImGui.EndTooltip()
+                end
+            end
+            ImGui.EndListBox()
+        end
+    end)
 end
