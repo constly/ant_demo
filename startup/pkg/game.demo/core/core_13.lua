dde7e828f0810ef278b6c221c33a9f5d7e5ac271
constly <constly@foxmail.com>
2024-02-15 21:51:29 +0800

    core_13: dump world

--- startup/pkg/game.demo/core/core_13.lua
+++ startup/pkg/game.demo/core/core_13.lua
@@ -14,20 +14,51 @@ local system = mgr.create_system(tbParam)
 local world = ecs.world
 local w = world.w
 local draw_world_state = require "utils.draw_world_state"
+local luaecs = import_package "ant.luaecs"
+local vfs = require "vfs"
+local showtip
+local path = vfs.repopath() .. '.app/temp/world_core_13.bin'
+
+function system.on_entry()
+end 
+
+function system.on_leave()
+end
 
 function system.data_changed()
 	ImGui.SetNextWindowPos(mgr.get_content_start())
 	ImGui.SetNextWindowSize(mgr.get_content_size())
 	if ImGui.Begin("window_body", nil, ImGui.WindowFlags {"NoResize", "NoMove", "NoScrollbar", "NoCollapse", "NoTitleBar"}) then 
-		ImGui.SetCursorPos(200, 200)
 
+		ImGui.SetCursorPos(200, 100)
+		ImGui.BeginGroup()
 		if ImGui.ButtonEx("打开: World Dump", 200, 60) then 
 			draw_world_state.open()
 		end
 
-		ImGui.Text("输出有多少entity, 多少system, 多少component, 占多少内存, 每帧耗时情况, 去3rd/luaecs下面找 ")
+		if ImGui.ButtonEx("将World写入到文件", 200, 60) then 
+			system.write_world_to_file()
+			showtip = true;
+			os.execute("code ".. path)
+		end
+		if showtip then 
+			ImGui.Text("文件存储至: " .. path)
+		end
+		--ImGui.Text("输出有多少entity, 多少system, 多少component, 占多少内存, 每帧耗时情况, 去3rd/luaecs下面找 ")
+
+		ImGui.EndGroup()
 
 		draw_world_state.draw(world)
 	end 
 	ImGui.End()
+end
+
+function system.write_world_to_file()
+	
+	luaecs.exporter.export(w, path)
+
+	-- 遍历所有entity
+	-- for v in w:select "eid:in" do
+	-- 	print(v.eid)
+	-- end
 end
