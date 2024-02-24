baff9d46bff1769214efc183ae85528acbdbaf89
constly <constly@foxmail.com>
2024-02-24 22:03:21 +0800

    add com.node.runtime

---
+++ startup/com/pkg/com.node.runtime.bp/main.lua
@@ -0,0 +1,8 @@
+local node_declare = require "def.bp_node_declare"
+
+---@class com.node.runtime.bp.api
+local api = {}
+
+api.node_declare = node_declare
+
+return api;
---
+++ startup/com/pkg/com.node.runtime/main.lua
@@ -0,0 +1,8 @@
+local node_declare = require "def.node_declare"
+
+---@class com.node.runtime.api
+local api = {}
+
+api.node_declare = node_declare
+
+return api;
