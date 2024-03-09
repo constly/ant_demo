52197b26392f1dbb942a9146db40e047f05daed7
constly <constly@foxmail.com>
2024-03-09 23:02:07 +0800

    功能: 局域网匹配开发中

--- startup/com/pkg/ly.room.match/room_list.lua
+++ startup/com/pkg/ly.room.match/room_list.lua
@@ -1,27 +1,45 @@
 --------------------------------------------------------------
 --- 局域网房间列表
 --------------------------------------------------------------
-local ltask = require "ltask"
 local ly_net = require 'ly.net'
-local api = {}  ---@class ly.room.match.room_list
+local common = import_package 'ly.common' ---@type ly.common.main
 local quit 
 local port = 17668
 local client
 local server
+local tb_rooms = {} ---@type ly.room.match.room_list_one[]
 
--- 每帧更新，获取局域网内广播信息
-local function loop()
-	while not quit do 
-		local ip, port, msg = client:receive()
-		if ip then 
-			print(string.format("ip:  %s \nport: %d \nmsg: %s", ip, port, msg))
+--------------------------------------------------------------
+--- 房间相关接口
+--------------------------------------------------------------
+---@return ly.room.match.room_list_one
+local function find_or_add_room(ip)
+	for i, v in ipairs(tb_rooms) do 
+		if v.ip == ip then 
+			return v
 		end
-		ltask.sleep(0)
-	end	
-	client:close()
-	client = nil
+	end
+	local tb = {}
+	tb.ip = ip
+	table.insert(tb_rooms, tb)
+	print("add room", ip)
+	return tb
 end
 
+local function remove_room(ip)
+	for i, v in ipairs(tb_rooms) do 
+		if v.ip == ip then 
+			table.remove(tb_rooms, i)
+			break
+		end
+	end
+	print("remove room", ip)
+end
+
+--------------------------------------------------------------
+--- api 接口
+--------------------------------------------------------------
+local api = {}  ---@class ly.room.match.room_list
 --- 初始化
 function api.init()
 	if client then return end 
@@ -30,25 +48,54 @@ function api.init()
 	if not client:init_client(port) then 
 		log.warn("failed to create broadcast client, error = " .. client:last_error())
 	end
-	ltask.fork(loop)
 end 
 
 --- 退出时
 function api.exit()
 	quit = true
+	tb_rooms = {}
 	if server then 
 		server:close()
 		server = nil
 	end 
+	if client then 
+		client:close()
+		client = nil
+	end 
+end 
+
+--- 更新局域网内房间列表
+function api.tick()
+	while client do 
+		local ip, port, msg = client:receive()
+		if ip then 
+			if msg == "close" then
+				remove_room(ip)
+			else 
+				local list = common.lib.split(msg, ";")
+				local room = find_or_add_room(ip)
+				for i, v in ipairs(list) do 
+					local arr = common.lib.split(v, ":");
+					if #arr == 2 then 
+						room[arr[1]] = arr[2] 
+					end 
+				end 
+				room.update_time = os.clock()
+			end
+		else
+			break; 
+		end 
+	end
 end 
 
 --- 得到房间列表
+---@return ly.room.match.room_list_one[] 
 function api.get_rooms()
-	return {}
+	return tb_rooms
 end 
 
 --- 发送房间信息到局域网
-function api.send_room_data(msg)
+function api.broadcast(msg)
 	if not server then 
 		server = ly_net.CreateBroadCast()
 		if not server:init_server("255.255.255.255", port) then 
--- startup/com/pkg/ly.room.match/room_system.lua
+++ startup/com/pkg/ly.room.match/room_system.lua
@@ -10,15 +10,23 @@ local room_data = require 'room_data'			---@type ly.room.match.room_data
 
 function system.init_world()
 	room_list.init()
+	room_data.init()
 end
 
 function system.exit()
-	room_list.exit()
 	room_data.close()
+	room_list.exit()
 	print("exit room_system")
 end 
 
 function system.data_changed()
+	room_data.tick()
+	if room_data.need_exit() then 
+		room_data.close()
+		show_type = 1
+	end
+	room_list.tick()
+
 	local viewport = ImGui.GetMainViewport();
 	local size_x, size_y = viewport.WorkSize.x, viewport.WorkSize.y
 	local region_x, region_y = 800, 550
@@ -44,13 +52,35 @@ function system.draw_room_list()
 	end
 
 	ImGui.SetCursorPos(60, 80)
-	ImGui.BeginChild("##child_1", x - 100, y - 180, ImGui.ChildFlags({"Border"}))
+	ImGui.BeginChild("##child_1", x - 100, y - 180, ImGui.ChildFlags({}))
+	do
+		local x, y = ImGui.GetContentRegionAvail()
+		local time = os.clock()
+		for i, v in ipairs(room_list.get_rooms()) do 
+			if time - v.update_time < 1 then
+				ImGui.BeginGroup()
+				ImGui.Text(tostring(i))
+				ImGui.SameLineEx(50)
+				ImGui.Text(string.format("%s:%s", v.ip, v.port))
+				ImGui.SameLineEx(150)
+				ImGui.Text(v.name or "")
+				ImGui.SameLineEx(x - 80)
+				if common.imgui_utils.draw_btn(" 加 入 ###btn_join" .. i, true) then 
+					if room_data.c2s_join(v.ip, v.port) then 
+						show_type = 2
+					end
+				end
+				ImGui.EndGroup()
+			end
+		end
+	end
 	ImGui.EndChild()
 
 	ImGui.SetCursorPos(x * 0.5 - 50, y - 50)
 	if common.imgui_utils.draw_btn(" 创建房间 ", true, {size_x = 100, size_y = 40}) then 
-		show_type = 2
-		room_data.create()
+		if room_data.create() then 
+			show_type = 2
+		end 
 	end
 end
 
@@ -58,10 +88,35 @@ function system.draw_room_data()
 	local x, y = ImGui.GetContentRegionAvail()
 	common.imgui_utils.draw_text_center(main.tbParam.name or "局域网联机")
 
+	ImGui.SetCursorPos(60, 80)
+	ImGui.BeginChild("##child_1", x - 100, y - 180, ImGui.ChildFlags({"Border"}))
+	do 
+		local x, y = ImGui.GetContentRegionAvail()
+		for i, member in ipairs(room_data.get_room_members()) do 
+			ImGui.BeginGroup()
+			ImGui.Text(tostring(i))
+			ImGui.SameLineEx(50)
+			ImGui.Text(member.name)
+			ImGui.EndGroup()
+		end
+	end 
+	ImGui.EndChild()
 
-	ImGui.SetCursorPos(x * 0.5 - 50, y - 50)
-	if common.imgui_utils.draw_btn(" 退 出 ", true, {size_x = 100, size_y = 40}) then 
-		show_type = 1
-		room_data.close()
+	if room_data.is_server() then 
+		ImGui.SetCursorPos(x * 0.5 - 120, y - 50)
+		if common.imgui_utils.draw_btn("解散房间", false, {size_x = 100, size_y = 40}) then 
+			show_type = 1
+			room_data.close()
+		end
+		ImGui.SameLine()
+		if common.imgui_utils.draw_btn(" 开 始 ", true, {size_x = 100, size_y = 40}) then 
+			room_data.begin()
+		end
+	else 
+		ImGui.SetCursorPos(x * 0.5 - 50, y - 50)
+		if common.imgui_utils.draw_btn(" 退 出 ", true, {size_x = 100, size_y = 40}) then 
+			show_type = 1
+			room_data.c2s_apply_exit()
+		end
 	end
 end
