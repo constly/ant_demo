75417dec718f64f50aa5304b2c1aa3fbef3752bd
constly <constly@foxmail.com>
2024-04-14 09:07:50 +0800

    开发: goap section 支持多选，进度 8539

--- startup/com/pkg/ly.game_editor/windows/goap/body_type_sections.lua
+++ startup/com/pkg/ly.game_editor/windows/goap/body_type_sections.lua
@@ -38,11 +38,50 @@ local function new(editor, stack, goap_handler, goap_render)
 
 	local function set_selected_inner(node, section_idx, lineIdx, i)
 		local cache = goap_handler.get_body_cache(node.id)
-		if not cache.selected or #cache.selected > 1 
-			or cache.selected[1] ~= section_idx or cache.selected[2] ~= lineIdx or cache.selected[3] ~= i then
-			cache.selected = section_idx and {{section_idx, lineIdx, i}} or {}
+		cache.is_shift = false
+		if not cache.selected or type(cache.selected) ~= "table" then cache.selected = {} end 
+		
+		if not cache.selected or #cache.selected ~= 1 
+			or cache.selected[1][1] ~= section_idx or cache.selected[1][2] ~= lineIdx or cache.selected[1][3] ~= i then
+			cache.selected = (section_idx and lineIdx) and {{section_idx, lineIdx, i}} or {}
 			return true
 		end
+	end
+
+	local function add_selected(node, section_idx, lineIdx, actionIdx)
+		local cache = goap_handler.get_body_cache(node.id)
+		if not cache.selected then cache.selected = {} end 
+		cache.is_shift = false
+		for _, v in ipairs(cache.selected) do 
+			if v[1] == section_idx and v[2] == lineIdx and v[3] == actionIdx then 
+				return false
+			end
+		end
+		for i = #cache.selected, 1, -1 do 
+			local v = cache.selected[i]
+			if not v[1] or v[1] ~= section_idx then 
+				table.remove(cache.selected, i)
+			end
+		end
+		table.insert(cache.selected, {section_idx, lineIdx, actionIdx})
+		return true
+	end
+
+	local function add_selected_shift(node, section_idx, lineIdx, actionIdx)
+		local cache = goap_handler.get_body_cache(node.id)
+		if not cache.selected or not section_idx or not lineIdx then 
+			return 
+		end
+		if type(cache.selected[1]) == "table" then 
+			local first = cache.selected[1]
+			if not first[1] or first[1] ~= section_idx then 
+				return
+			end
+			cache.selected = {first, {section_idx, lineIdx, actionIdx}}
+		else 
+			cache.selected = {{section_idx, lineIdx, actionIdx}}
+		end 
+		cache.is_shift = true
 	end
 
 	local function set_selected(node, section_idx, lineIdx, i)
@@ -52,9 +91,35 @@ local function new(editor, stack, goap_handler, goap_render)
 
 	local function is_selected(node, section_idx, lineIdx, actionIdx)
 		local cache = goap_handler.get_body_cache(node.id)
-		for _, _v in ipairs(cache.selected or {}) do 
-			if _v[1] == section_idx and _v[2] == lineIdx and _v[3] == actionIdx then 
-				return true
+		if not cache.selected then return end 
+
+		for i = #cache.selected, 1, -1 do 
+			local v = cache.selected[i]
+			if not v or not v[1] then 
+				table.remove(cache.selected, i)
+			end
+		end
+
+		if cache.is_shift and #cache.selected == 2 then 
+			local first = cache.selected[1]
+			local second = cache.selected[2]
+			if first[1] ~= section_idx then 
+				return false 
+			end 
+
+			local min_line = math.min(first[2], second[2])
+			local max_line = math.max(first[2], second[2])
+			local min_action = math.min(first[3], second[3])
+			local max_action = math.max(first[3], second[3])
+			if not min_line or not max_line or not min_action or not max_action then 
+				return false
+			end
+			return lineIdx >= min_line and lineIdx <= max_line and actionIdx >= min_action and actionIdx <= max_action
+		else
+			for _, _v in ipairs(cache.selected or {}) do 
+				if _v[1] == section_idx and _v[2] == lineIdx and _v[3] == actionIdx then 
+					return true
+				end
 			end
 		end
 	end
@@ -88,10 +153,18 @@ local function new(editor, stack, goap_handler, goap_render)
 				style = selected and GStyle.btn_left_selected or style
 				local desc = editor.tbParams.goap_mgr.get_action_desc(action)
 				local label = string.format("%s##btn_line_%d_%d_%d", desc or "", section_idx, lineIdx, i)
-				if editor.style.draw_style_btn(label, style, {size_x = item_len_x}) then 
-					if set_selected(node, section_idx, lineIdx, i) then 
-						stack.snapshoot(false)
+				if editor.style.draw_style_btn(label, style, {size_x = item_len_x}) then
+					local ok = false 
+					if ImGui.IsKeyDown(ImGui.Key.LeftCtrl) then 
+						ok = add_selected(node, section_idx, lineIdx, i)
+					elseif ImGui.IsKeyDown(ImGui.Key.LeftShift) then 
+						ok = add_selected_shift(node, section_idx, lineIdx, i)
+					else
+						ok = set_selected(node, section_idx, lineIdx, i)
 					end 
+					if ok then 
+						stack.snapshoot(false)
+					end
 				end
 				if ImGui.IsItemHovered() and ImGui.IsMouseDoubleClicked(ImGui.MouseButton.Left) then 
 					---@type ly.game_editor.action.selector.params
