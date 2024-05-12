a085ba20d2d4ff1fe1cda5effe1679f4b82f3ae1
constly <constly@foxmail.com>
2024-05-12 11:06:58 +0800

    开发: pc打包持续完善中

--- run_pack.bat
+++ run_pack.bat
@@ -1,3 +1,20 @@
 :: PC打包
 chcp 65001
-"./bin/msvc/debug/ant_demo.exe" -p
+
+set current_dir=%~dp0
+
+set mode=%1
+if not defined mode (
+	set mode=release
+)
+
+luamake -mode %mode%
+luamake -mode release tools
+"./bin/msvc/%mode%/ant_demo.exe" -p
+
+set src_dir=%current_dir%bin\msvc\%mode%\
+set dest_dir=%current_dir%publish
+
+copy %src_dir%ant_demo.exe %dest_dir%
+copy %src_dir%fmod.dll %dest_dir%
+copy %src_dir%fmodstudio.dll %dest_dir%
