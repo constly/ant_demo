e6ae62bebe2dc3ebefa38c81ff85cd66408755d3
constly <constly@foxmail.com>
2024-05-13 23:52:56 +0800

    开发: 同步mod下面的文件到vfs中

--- startup/com/pkg/ly.mod/mods.lua
+++ startup/com/pkg/ly.mod/mods.lua
@@ -14,20 +14,28 @@ local function new()
 	local data = {}
 	
 	local memfs = import_package "ant.vfs".memory
+	memfs.init()
+
+	---@type string 根目录
+	local root_path = nil
 
 	---@param tbParams ly.mods.param
 	function api.init(tbParams)
 		local fs = require "filesystem"
 		local bfs = require 'bee.filesystem'
+		root_path = tbParams.root
+		print("set root path", root_path)
+
 		-- fs遍历vfs目录
 		-- bfs遍历本地目录
 
-		for item in bfs.pairs(tbParams.root) do
-			print("init", tostring(item))
+		for file, file_status in bfs.pairs(root_path) do
+			if file_status:is_directory() then
+				local pkg_name = file:filename():string()
+				data[pkg_name] = true
+				api.refresh_pkg(pkg_name)
+			end
 		end
-
-		--memfs.update("/pkg/testmem/test.txt", "testmem.txt")
-
 	end
 
 	--- 判断包是不是mod包
@@ -49,6 +57,27 @@ local function new()
 			table.insert(rets, name)
 		end
 		return rets
+	end
+
+	-- 刷新指定pkg
+	function api.refresh_pkg(pkg_name)
+		memfs.remove("/pkg/" .. pkg_name)
+
+		local bfs = require 'bee.filesystem'
+		local function init_files(path, short_path)
+			for file, file_status in bfs.pairs(path) do
+				local file_name = file:filename():string()
+				if file_status:is_directory() then
+					init_files(tostring(file), short_path .. file_name .. "/");
+				else
+					local vfs_path = short_path .. file_name
+					local full_path = file:string()
+					print("add file", vfs_path, full_path)
+					memfs.update(vfs_path, full_path)
+				end	
+			end
+		end
+		init_files(root_path .. "/" .. pkg_name, "/pkg/" .. pkg_name .. "/")
 	end
 
 	-- 刷新指定目录
