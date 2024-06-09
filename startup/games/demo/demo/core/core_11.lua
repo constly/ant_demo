e68fbe29331adbf15ee52e124d2aaaa17d5d3f35
constly <constly@foxmail.com>
2024-06-09 14:41:46 +0800

    demo: 四面体绘制

--- startup/games/demo/demo/core/core_11.lua
+++ startup/games/demo/demo/core/core_11.lua
@@ -19,6 +19,8 @@ local math3d = require "math3d"
 local imaterial = ecs.require "ant.render|material"
 local PC  = ecs.require("utils.world_handler").proxy_creator()
 local imesh 	= ecs.require "ant.asset|mesh"
+local iom   = ecs.require "ant.objcontroller|obj_motion"
+local timer 	= ecs.require "ant.timer|timer_system"
 local renderpkg	= import_package "ant.render"
 local layoutmgr = renderpkg.layoutmgr
 
@@ -40,7 +42,8 @@ function system.on_entry()
 		policy = { "ant.render|simplerender" },
 		data = {
 			scene 		= {
-				s = {5, 1, 5},	-- 缩放
+				s = {0.5, 0.5, 0.5},	-- 缩放
+				t = {-2, 0, 0},
             },
 			material 	= "/pkg/ant.resources/materials/mesh_shadow.material",
 			visible	= true,
@@ -56,32 +59,62 @@ function system.on_entry()
             "ant.render|simplerender",
         },
         data = {
-            scene = {s = 0.5, t = {0, 1, 0}},
+            scene = {s = 0.5, t = {-1, 0, 0}},
             mesh_result = system.create_cube(),
-            material    = "/pkg/ant.resources/materials/meshcolor.material",
+            material    = "/pkg/ant.resources/materials/mesh_shadow.material",
             visible     = true,
             on_ready = function (e)
-                imaterial.set_property(e, "u_color", math3d.vector(1, 1, 1))
+                imaterial.set_property(e, "u_basecolor_factor", math3d.vector(1, 1, 1))
             end,
         },
     }
 
-	world:create_entity{
+	PC:create_entity{
 		policy = {
 			"ant.render|simplerender",
 		},
 		data = {
-			scene 		= {s = 0.5, t = {-1, 0, -1}},
+			scene 		= {s = 0.5, t = {0, 0, 0}},
 			material	= "/pkg/ant.resources/materials/line.material",
 			render_layer= "translucent",
 			mesh_result	= system.create_axis(),
 			visible		= true,
 		}
 	}
+
+	for i, v in ipairs({"tetrahedron", "hexahedron"}) do 
+		PC:create_entity {
+			policy = {
+				"ant.render|simplerender",
+			},
+			data = {
+				scene = {s = 0.5, t = {i, 0, 0}},
+				mesh_result = system[string.format("create_%s", v)](),
+				material    = "/pkg/ant.resources/materials/meshcolor.material",
+				visible     = true,
+				on_ready = function (e)
+					imaterial.set_property(e, "u_color", math3d.vector(1, 1, 1))
+				end,
+			},
+		}
+	end
 end 
 
 function system.on_leave()
 	PC:clear()
+end
+
+local time = 0;
+function system.data_changed()
+	local delta_time = timer.delta() * 0.001
+	time = time + delta_time
+	for idx, eid in ipairs(PC._entities) do 
+		local e<close> = world:entity(eid)
+		if e then 
+			local rot = math3d.quaternion({time + idx * 0.2, time + idx * 0.35, 0})
+			iom.set_rotation(e, rot)
+		end
+	end
 end
 
 local function create_mesh(vbdata, ibdata, aabb)
@@ -177,4 +210,68 @@ function system.create_axis()
 		0, 0, 1, b[1], b[2], b[3], b[4],
 	}
 	return create_mesh{"p3|c4", axis_vb}
+end
+
+--- 绘制正四面体
+--- https://danielsieger.com/blog/2021/01/03/generating-platonic-solids.html
+function system.create_tetrahedron(u0, v0, u1, v1)
+	if not u0 then
+		u0, v0, u1, v1 = 0, 0, 1, 1
+	end
+
+	local a = 1 / 3
+	local b = math.sqrt(8 / 9)
+	local c = math.sqrt(2 / 9)
+	local d = math.sqrt(2 / 3)
+	local vb = {
+		0, 	0, 	1, 	0, 1, 0, u0, v0,
+		-c, d, -a, 0, 1, 0, u1, v0,
+		-c, -d, -a, 0, 1, 0, u0, v1,
+		b, 0, -a, 0, 1, 0, u1, v1,		
+	}
+	local vbdata = {"p3|n3|t2", vb}
+	local ibdata = {
+		0, 1, 2,
+		0, 2, 3,
+		0, 3, 1,
+		3, 2, 1
+	}
+	local aabb = {{-1, -1, 1}, {1, 1, 1}}
+	return create_mesh(vbdata, ibdata, aabb)
+end
+
+--- 绘制正六面体
+--- https://danielsieger.com/blog/2021/01/03/generating-platonic-solids.html
+function system.create_hexahedron(u0, v0, u1, v1)
+	local a = 1 / math.sqrt(3)
+	local vb = {
+		-a, -a, -a, 
+		a, -a, -a,
+		a, a, -a,
+		-a, a, -a,
+		-a, -a, a,
+		a, -a, a,
+		a, a, a,
+		-a, a, a,
+	}
+	local vbdata = {"p3", vb}
+	local ibdata = {}
+	local add_quad = function(a, b, c, d)
+		local n = #ibdata
+		ibdata[n + 1] = a
+		ibdata[n + 2] = b
+		ibdata[n + 3] = c
+
+		ibdata[n + 4] = b
+		ibdata[n + 5] = d
+		ibdata[n + 6] = c
+	end
+	add_quad(3, 2, 1, 0)
+	add_quad(2, 6, 5, 1)
+	add_quad(5, 6, 7, 4)
+	add_quad(0, 4, 7, 3)
+	add_quad(3, 7, 6, 2)
+	add_quad(1, 5, 4, 0)
+	local aabb = {{-1, -1, 1}, {1, 1, 1}}
+	return create_mesh(vbdata, ibdata, aabb)
 end
