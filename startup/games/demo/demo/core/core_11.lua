c9204cf200f08be5a31f55b8874fe8ea192f2e5b
constly <constly@foxmail.com>
2024-06-09 11:01:11 +0800

    demo: cube绘制研究中

--- startup/games/demo/demo/core/core_11.lua
+++ startup/games/demo/demo/core/core_11.lua
@@ -18,6 +18,9 @@ local icamera = ecs.require "ant.camera|camera"
 local math3d = require "math3d"
 local imaterial = ecs.require "ant.render|material"
 local PC  = ecs.require("utils.world_handler").proxy_creator()
+local imesh 	= ecs.require "ant.asset|mesh"
+local renderpkg	= import_package "ant.render"
+local layoutmgr = renderpkg.layoutmgr
 
 function system.on_entry()
 	PC:create_instance { 
@@ -32,15 +35,16 @@ function system.on_entry()
 		end
 	}
 
+	-- 怎么给panel/cube设置贴图？
 	PC:create_entity{
-		policy = { "ant.render|render" },
+		policy = { "ant.render|simplerender" },
 		data = {
 			scene 		= {
 				s = {5, 1, 5},	-- 缩放
             },
 			material 	= "/pkg/ant.resources/materials/mesh_shadow.material",
 			visible	= true,
-			mesh = "plane.primitive",
+			mesh_result = system.create_plane(),
 			on_ready = function(e)
 				imaterial.set_property(e, "u_basecolor_factor", math3d.vector(0.3, 0.3, 0.3))
 			end
@@ -54,48 +58,123 @@ function system.on_entry()
         data = {
             scene = {s = 0.5, t = {0, 1, 0}},
             mesh_result = system.create_cube(),
-            material    = "/pkg/ant.resources/materials/mesh_shadow.material",
+            material    = "/pkg/ant.resources/materials/meshcolor.material",
             visible     = true,
-            visible_masks = "main_view|selectable",
             on_ready = function (e)
-                imaterial.set_property(e, "u_basecolor_factor", math3d.vector(1, 1, 0))
+                imaterial.set_property(e, "u_color", math3d.vector(1, 1, 1))
             end,
         },
     }
+
+	world:create_entity{
+		policy = {
+			"ant.render|simplerender",
+		},
+		data = {
+			scene 		= {s = 0.5, t = {-1, 0, -1}},
+			material	= "/pkg/ant.resources/materials/line.material",
+			render_layer= "translucent",
+			mesh_result	= system.create_axis(),
+			visible		= true,
+		}
+	}
 end 
 
 function system.on_leave()
 	PC:clear()
 end
 
-function system.create_cube()
-	local vdecl = bgfx.vertex_layout {
-		{ "POSITION", 3, "FLOAT" },
-		{ "COLOR0", 4, "UINT8", true },
+local function create_mesh(vbdata, ibdata, aabb)
+	local vb = {
+		start = 0,
 	}
+	local mesh = {vb = vb}
 
-	local buf = bgfx.memory_buffer(16 * 8)
-	buf[1]    = string.pack("fffL", -1.0,  1.0,  1.0, 0xff000000)
-	buf[16+1] = string.pack("fffL", 1.0,  1.0,  1.0, 0xff0000ff)
-	buf[32+1] = string.pack("fffL", -1.0, -1.0,  1.0, 0xff00ff00)
-	buf[48+1] = string.pack("fffL", 1.0, -1.0,  1.0, 0xff00ffff)
-	buf[64+1] = string.pack("fffL", -1.0,  1.0, -1.0, 0xffff0000)
-	buf[80+1] = string.pack("fffL", 1.0,  1.0, -1.0, 0xffff00ff)
-	buf[96+1] = string.pack("fffL", -1.0, -1.0, -1.0, 0xffffff00)
-	buf[112+1] = string.pack("fffL", 1.0, -1.0, -1.0, 0xffffffff)
+	if aabb then
+		mesh.bounding = {aabb=aabb}
+	end
+	
+	local correct_layout = layoutmgr.correct_layout(vbdata[1]) -- 'p30NIf|n30NIf|t20NIf'
+	local flag = layoutmgr.vertex_desc_str(correct_layout)
 
-	return {
-		vb = {
-			start = 0,
-			num = 128,
-			handle = bgfx.create_vertex_buffer(buf, vdecl)
-		},
-		ib = {
-			start = 0,
-			num = 14,
-			handle = bgfx.create_index_buffer{
-				0, 1, 2, 3, 7, 1, 5, 0, 4, 2, 6, 7, 4, 5,
-			}
+	vb.num = #vbdata[2] // #flag
+	vb.declname = correct_layout
+	vb.memory = {flag, vbdata[2]}
+
+	if ibdata then
+		mesh.ib = {
+			start = 0, num = #ibdata,
+			memory = {"w", ibdata},
 		}
+	end
+	return imesh.init_mesh(mesh)
+end
+
+function system.create_plane(u0, v0, u1, v1)
+	if not u0 then
+		u0, v0, u1, v1 = 0, 0, 1, 1
+	end
+	local vb = {
+		-0.5, 0, 0.5, 0, 1, 0, u0, v0,	--left top
+		 0.5, 0, 0.5, 0, 1, 0, u1, v0,	--right top
+		-0.5, 0,-0.5, 0, 1, 0, u0, v1,	--left bottom
+		 0.5, 0,-0.5, 0, 1, 0, u1, v1,	--right bottom
 	}
+	local vbdata = {"p3|n3|t2", vb}
+	local ibdata = {0, 1, 2, 1, 3, 2}   -- 以顺时针方向绘制三角面
+	local aabb = {{-0.5, 0, -0.5}, {0.5, 0, 0.5}}
+	return create_mesh(vbdata, ibdata, aabb)
+end
+
+function system.create_cube(u0, v0, u1, v1)
+	if not u0 then
+		u0, v0, u1, v1 = 0, 0, 1, 1
+	end
+	local vb = {
+													-- z = 0.5
+		-0.5, 	0.5, 	0.5, 	0, 1, 0, u0, v0,	--left top
+		0.5, 	0.5, 	0.5, 	0, 1, 0, u1, v0,	--right top
+		-0.5, 	-0.5, 	0.5, 	0, 1, 0, u0, v1,	--left bottom
+		0.5, 	-0.5, 	0.5, 	0, 1, 0, u1, v1,	--right bottom
+
+													-- z = -0.5
+		-0.5, 	0.5, 	-0.5, 	0, 1, 0, u0, v0,	--left top
+		0.5, 	0.5, 	-0.5, 	0, 1, 0, u1, v0,	--right top
+		-0.5, 	-0.5, 	-0.5, 	0, 1, 0, u0, v1,	--left bottom
+		0.5, 	-0.5, 	-0.5, 	0, 1, 0, u1, v1,	--right bottom
+
+	}
+	local vbdata = {"p3|n3|t2", vb}
+	--local ibdata = {0, 1, 2, 3, 7, 1, 5, 0, 4, 2, 6, 7, 4, 5,}  
+	local ibdata = {
+		0, 1, 2, -- 0
+		1, 3, 2,
+		4, 6, 5, -- 2
+		5, 6, 7,
+		0, 2, 4, -- 4
+		4, 2, 6,
+		1, 5, 3, -- 6
+		5, 7, 3,
+		0, 4, 1, -- 8
+		4, 5, 1,
+		2, 3, 6, -- 10
+		6, 3, 7,
+	}
+	local aabb = {{-0.5, -0.5, -0.5}, {0.5, 0.5, 0.5}}
+	return create_mesh(vbdata, ibdata, aabb)
+end
+
+function system.create_axis()
+	local r = {1, 0, 0, 1} 
+	local g = {0, 1, 0, 1}
+	local b = {0, 0, 1, 1}
+	local axis_vb = {
+		0, 0, 0, r[1], r[2], r[3], r[4],
+		1, 0, 0, r[1], r[2], r[3], r[4],
+		0, 0, 0, g[1], g[2], g[3], g[4],
+		0, 1, 0, g[1], g[2], g[3], g[4],
+		0, 0, 0, b[1], b[2], b[3], b[4],
+		0, 0, 1, b[1], b[2], b[3], b[4],
+	}
+	return create_mesh{"p3|c4", axis_vb}
 end
