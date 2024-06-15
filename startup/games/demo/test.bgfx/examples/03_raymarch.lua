dda1f0db8f9c99591deb3aefc4c7d96c3dac996c
constly <constly@foxmail.com>
2024-06-15 19:42:35 +0800

    bgfx: 03

---
+++ startup/games/demo/test.bgfx/examples/03_raymarch.lua
@@ -0,0 +1,23 @@
+local bgfx    = require "bgfx"	---@type bgfx
+local ImGui 	= require "imgui"
+local viewid = 2
+
+local math3d    = require "math3d"
+local utils		= require 'utils' ---@type test.bfgx.utils
+
+---@type test.bgfx.example 
+local api 		= {}
+
+function api.on_entry()
+
+end 
+
+function api.on_exit()
+end 
+
+function api.on_resize()
+end 
+
+function api.update(delta_time)
+	
+end
